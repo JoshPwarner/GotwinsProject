f5a04d44772ee8a40841787ddc6ff5001284c3e9
Joshua Warner <joshuawarner51@yahoo.com>
2022-07-07 15:49:11 +0100

    done some heroku stuff

--- app.js
+++ app.js
@@ -23,10 +23,18 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/', express.static('./index.html'));
+app.use(express.static('./client'));
+
+app.use(express.static('./data.json'));
+
+app.use( express.static('./client/assets/css/style.css'));
+
+app.use( express.static('./client/assets/js/'));
+
+app.use( express.static('./client/assets/images'));
 
 app.get('/', (req, res) => {
-    res.send(path.resolve(__dirname, './index.html'));
+    res.sendFile(path.resolve(__dirname, './index.html'));
 })
 
 
