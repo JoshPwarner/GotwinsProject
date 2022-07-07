fa133d17e747f8f46c24d3c8c01dcdf3e1ce2ae8
Joshua Warner <joshuawarner51@yahoo.com>
2022-07-07 15:30:59 +0100

    server changes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const urlEncodedParser = bodyParser.urlencoded({extended: false});
 const port = process.env.PORT || 5000
+const path = require("path");
 
 
 app.use('/api/posts', postsAndComments);
@@ -22,9 +23,12 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use('/', express.static('./index.html'));
+
 app.get('/', (req, res) => {
-    res.send('This is my axios test homepage');
+    res.send(path.resolve(__dirname, './index.html'));
 })
+
 
 
 app.listen(port, () => {
